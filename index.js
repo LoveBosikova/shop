549d0affe85dc282aaea60b1bba79152f73273d6
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-05-13 20:13:40 +0300

    totally fixed

--- index.js
+++ index.js
@@ -11,26 +11,10 @@ let totalPrice =  document.getElementById('totalPrice');
 totalPrice.textContent = (firstPrice + secondPrice + thirdPrice + fourthPrice);
 
 let summary = totalPrice.textContent;
-console.log(summary);
-console.log(typeof summary);
-//здесь саммари-это строка
 
-const countSale = (summary) => {
-    console.log(summary);
-    console.log(typeof summary); //здесь саммари - PointerEvent 
-    
-    summary = Number(summary);
-    console.log(summary);
-    console.log(typeof summary); 
-    // а здесь NaN, обьект так не приводится к числу
-    
-    summary = (summary*.8);
-    console.log(summary);
-    console.log(typeof summary);
+let discountPrice = ((Number(summary))*.8);
 
-    totalPrice.textContent = summary;
+const countSale = () => {
+    totalPrice.textContent = discountPrice.toFixed();
 };
 
-const button = document.getElementById("button");
-
-button.addEventListener('click', countSale);
