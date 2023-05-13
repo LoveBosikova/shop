7c86ae9f54d252cb1560dfa72d01acc2d96eef68
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-05-13 20:18:49 +0300

    totally fixed

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const firstPrice = +(document.getElementById('firstPrice').textContent);
-
 const secondPrice = +(document.getElementById('secondPrice').textContent);
-
 const thirdPrice = +(document.getElementById('thirdPrice').textContent);
-
 const fourthPrice = +(document.getElementById('fourthPrice').textContent);
 
 let totalPrice =  document.getElementById('totalPrice');
